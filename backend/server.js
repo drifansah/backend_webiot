d6aaab0d4ccbb21675f567fdf5b22ec73fe8a245
drifansah <127324766+drifansah@users.noreply.github.com>
2025-07-03 01:46:43 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.json());
+
 
 // Simpan status lampu di memori (sifatnya sementara)
 let status = {
